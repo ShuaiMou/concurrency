d7da4731f3567cb1822863ee90bd1d57e8ac7960
ShuaiMou <shuaimou77@gmail.com>
2020-04-24 20:35:43 +1000

    测试 atomic 包里的相应类

---
+++ src/main/java/com/unimelb/concurrency/atomic/AtomicIntegerFieldUpdaterTest.java
@@ -0,0 +1,33 @@
+package com.unimelb.concurrency.atomic;
+
+import com.unimelb.concurrency.annotations.ThreadSafe;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+@Slf4j
+@ThreadSafe
+public class AtomicIntegerFieldUpdaterTest {
+
+    private static AtomicIntegerFieldUpdater<AtomicIntegerFieldUpdaterTest> updater =
+            AtomicIntegerFieldUpdater.newUpdater(AtomicIntegerFieldUpdaterTest.class, "count");
+
+    @Getter
+    public volatile int count = 100;
+
+    public static void main(String[] args) {
+
+        AtomicIntegerFieldUpdaterTest example5 = new AtomicIntegerFieldUpdaterTest();
+
+        if (updater.compareAndSet(example5, 100, 120)) {
+            log.info("update success 1, {}", example5.getCount());
+        }
+
+        if (updater.compareAndSet(example5, 100, 120)) {
+            log.info("update success 2, {}", example5.getCount());
+        } else {
+            log.info("update failed, {}", example5.getCount());
+        }
+    }
+}
