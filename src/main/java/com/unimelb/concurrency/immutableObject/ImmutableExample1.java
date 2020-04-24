3698901c27705e99e6c32521154ca9dfec8bd173
ShuaiMou <shuaimou77@gmail.com>
2020-04-25 09:58:13 +1000

    不可变类，final 的使用，Collections.unmodifiableXXX，

---
+++ src/main/java/com/unimelb/concurrency/immutableObject/ImmutableExample1.java
@@ -0,0 +1,36 @@
+package com.unimelb.concurrency.immutableObject;
+
+import com.google.common.collect.Maps;
+import com.unimelb.concurrency.annotations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+
+@Slf4j
+@NotThreadSafe
+public class ImmutableExample1 {
+
+    private final static Integer a = 1;
+    private final static String b = "2";
+    private final static Map<Integer, Integer> map = Maps.newHashMap();
+
+    static {
+        map.put(1, 2);
+        map.put(3, 4);
+        map.put(5, 6);
+    }
+
+    public static void main(String[] args) {
+//        a = 2;
+//        b = "3";
+//        map = Maps.newHashMap();
+
+        //final修饰的引用不能指向新的对象引用，但是可以修改对象的值
+        map.put(1, 3);
+        log.info("{}", map.get(1));
+    }
+
+    private void test(final int a) {
+//        a = 1;
+    }
+}
