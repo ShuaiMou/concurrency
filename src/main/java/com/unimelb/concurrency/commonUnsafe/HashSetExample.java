df7856d3b290bc4489edc592e5f5595b6f2a9e3d
ShuaiMou <shuaimou77@gmail.com>
2020-04-25 11:33:42 +1000

    测试线程不安全类ArrayList, HashMap, HashSet

--- src/main/java/com/unimelb/concurrency/commonUnsafe/HashSetExample.java
+++ src/main/java/com/unimelb/concurrency/commonUnsafe/HashSetExample.java
@@ -1,6 +1,6 @@
-package com.mmall.concurrency.example.commonUnsafe;
+package com.unimelb.concurrency.commonUnsafe;
 
-import com.mmall.concurrency.annoations.NotThreadSafe;
+import com.unimelb.concurrency.annotations.NotThreadSafe;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashSet;
